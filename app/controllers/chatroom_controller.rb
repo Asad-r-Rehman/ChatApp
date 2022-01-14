81b243b9d7e2c7ab9d01972e81515193b23d7adc
Asad Ur Rehman <asadsmd000@gmail.com>
2022-01-14 12:42:13 +0500

    re-facotore code

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,5 +1,6 @@
 class ChatroomController < ApplicationController
-  # after_action
+  after_action :admin_right, only: ['create']
+
   def index
     @users = User.all
     user = current_user
@@ -18,9 +19,6 @@ class ChatroomController < ApplicationController
     chatroom = Chatroom.find(params[:format])
     chatroom.users.delete(user)
   end
-  # def admin_right
-  #   admin  current_user
-  # end
 
   def destroy
     @chatroom = Chatroom.find(params[:id])
@@ -34,15 +32,23 @@ class ChatroomController < ApplicationController
       group_name = params[:group_title].empty? ? "No Name" : params[:group_title]
       @chatroom = Chatroom.new
       @chatroom.description = group_name
-      if @chatroom.save!
-        ChatroomUser.create(user_id: current_user.id , chatroom_id: @chatroom.id, admin_role: true )
-      end
+      @chatroom.save!
       params[:group_members].each do |ids|
         @chatroom.chatroom_users.create(user_id: ids)
         flash[:alert] = "Successfully Created"
       end
     else
       flash[:alert] = "Please add at lease one member"
+    end
+  end
+
+  private
+
+  def admin_right
+    user_right = ChatroomUser.find_by(user_id: current_user.id, chatroom_id: @chatroom.id)
+    user_right.admin_role = true
+    if user_right.save!
+      redirect_to chatroom_index_path
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :chatroom
   resources :message
-
+  get 'admin_right', to: 'chatroom#admin_right'
   get "chatArea" , to: "chatroom#chatarea"
   root "session#new"
   mount ActionCable.server, at: '/cable'
